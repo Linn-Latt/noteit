90b8eb361425af469d9f72fb7a5810f4195d01d4
Liv <linnlatt.ww11@gmail.com>
2026-05-21 23:22:45 +0630

    check email validation using Zerobounce

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,14 +1,43 @@
 import { betterAuth } from "better-auth";
+import { createAuthMiddleware, APIError } from "better-auth/api";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma";
-// import { PrismaClient } from "@/app/generated/prisma/client";
-// import { PrismaNeon } from "@prisma/adapter-neon";
-
-// const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
-// const prisma = new PrismaClient({ adapter });
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, { provider: "postgresql" }),
     emailAndPassword: { enabled: true },
     baseURL: process.env.BETTER_AUTH_URL,
+    hooks: {
+        before: createAuthMiddleware(async (ctx) => {
+            if (ctx.path === "/sign-up/email") {
+                const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                const email = ctx.body?.email;
+
+                if (typeof email === "string") {
+                    if (!emailRegex.test(email)) {
+                        throw new APIError("BAD_REQUEST", { message: "Invalid email format" });
+                    }
+
+                    const disposableDomains = ["tempmail.com", "mailinator.com"];
+                    const domain = email.split('@')[1];
+                    if (domain && disposableDomains.includes(domain)) {
+                        throw new APIError("BAD_REQUEST", { message: "Disposable email addresses are not allowed" });
+                    }
+
+                    const zbRes = await fetch(
+                        `https://api.zerobounce.net/v2/validate?api_key=${process.env.ZEROBOUNCE_API_KEY}&email=${encodeURIComponent(email)}&ip_address=`
+                    );
+                    const zbData = await zbRes.json();
+
+                    if (
+                        zbData.status === "invalid" ||
+                        zbData.status === "spamtrap" ||
+                        zbData.status === "do_not_mail"
+                    ) {
+                        throw new APIError("BAD_REQUEST", { message: "Email address is invalid or doesn't exist." });
+                    }
+                }
+            }
+        })
+    }
 });
